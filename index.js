1b19e786850879fc7487015acd39f204c385c894
Moulham Hallak <mou990@gmail.com>
2021-05-20 13:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Write your code here
 
 class Breakfast {
-  // constructor(name, breed) {
-  // this.name = name;
-  // this.breed = breed;
-  // }
+  constructor(name, breed) {
+  this.name = name;
+  this.breed = breed;
+  }
 }
 
 let newBreakfast = new Breakfast('cheese', 'egg');
+
 class Lunch {
   
 }
